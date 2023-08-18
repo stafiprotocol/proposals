f29a8d0ef44f9eeedb2789ac241145ecfc2c41f4
曹操 <xuzhao.sgn@gmail.com>
2023-08-18 11:22:13 +0800

    fix scripts

--- scripts/uploader.js
+++ scripts/uploader.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /* eslint-disable */
-import * as dotenv from "dotenv"
+const dotenv = require("dotenv");
 import bs58 from "bs58"
 import { execSync } from "child_process"
 import { exit } from "process"
