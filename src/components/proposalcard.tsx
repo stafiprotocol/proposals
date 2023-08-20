e3fad448a3569409b1a498628ce15f5aa668fe14
曹操 <xuzhao.sgn@gmail.com>
2023-08-20 21:13:28 +0800

    support has

--- src/components/proposalcard.tsx
+++ src/components/proposalcard.tsx
@@ -1,10 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 import * as React from 'react';
-import Status from './status';
-import { IpfsIcon,SnapshotIcon,ArrowIcon } from './icons';
+import Status from '@/components/status';
+import { IpfsIcon,SnapshotIcon,ArrowIcon } from '@/components/icons';
 import Link from 'next/link';
 import { GetStaticProps } from 'next';
+import jsonDataList from '../../cid/ipfs-sips/all-hash.json';
 
 
 interface ProposalCard {
@@ -17,9 +18,11 @@ interface ProposalCard {
 
 export const ProposalCard = (params: ProposalCard) => {
 
-  
-  const matchedItem = jsonDataList.find(item => item.name === params.title);
+  console.log(jsonDataList)
+  const matchedItem = jsonDataList.find(item => `/proposal/${item.name}` === params.url);
+  console.log(matchedItem)
   const hashlink = matchedItem ? matchedItem.hash : '';
+  console.log(hashlink)
 
     return ( 
       <div className='max-w-3xl mx-auto'>
@@ -40,7 +43,7 @@ export const ProposalCard = (params: ProposalCard) => {
             </p>
           </Link>
           <div className='grid grid-cols-2 gap-4 mt-4'>
-            { hashlink && <Link href='{`https://ipfs.io/ipfs/${hashlink}`}'>
+            { hashlink && <Link href={`https://ipfs.io/ipfs/${hashlink}`} target='_blank'>
               <div className='flex items-center space-x-4 bg-[#E9EAEE]/30 p-2 rounded-md'>
                 <div className='rounded-full bg-[#E9EAEE] p-1.5'>
                   <IpfsIcon/>
